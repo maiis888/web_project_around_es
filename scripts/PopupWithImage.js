a0b70ca4ed0a467340eae6d58b650f3d32c1b2cc
maiis888 <marianausabiaga@gmail.com>
2026-03-18 01:41:31 +0100

    Add PopupWithImage.js

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,19 @@
+import { Popup } from "./Popup";
+
+/*Esta clase tiene que cambiar el método padre open().
+En el método open() de la clase PopupWithImage, debes añadir una imagen al popup y el correspondiente atributo de imagen src junto con una leyenda para la imagen.*/
+
+class PopupWithImage extends Popup {
+    constructor (popupSelector){
+        super(popupSelector);
+    }
+
+open({imageName, imageLink}){
+    const imageElement = this._popup.querySelector(".popup__image");
+    const captionElement = this._popup.querySelector(".popup__caption")
+    imageElement.alt = imageName;
+    imageElement.src = imageLink;
+    captionElement.textContent = imageName;
+    super.open();
+}
+}
